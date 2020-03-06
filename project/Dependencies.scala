a183cd1af47330e70602dbf002a6e7f06265bbce
Rene Ruck <TheGenuine@users.noreply.github.com>
2020-03-06 15:08:09 +0000

    Update silencer-plugin to 1.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,11 +3,11 @@ import sbt._
 object Dependencies {
   lazy val catsVersion = "2.1.1"
 
-  lazy val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.30" exclude("org.slf4j", "slf4j-log4j12")
+  lazy val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.30" exclude ("org.slf4j", "slf4j-log4j12")
 
   // Javax excluded and added specifially to avoid https://github.com/sbt/sbt/issues/3618
   // included explicitly to let us deal with vulns
-  lazy val kafka = "org.apache.kafka" % "kafka-clients" % "2.4.0" exclude("org.slf4j", "slf4j-log4j12")
+  lazy val kafka        = "org.apache.kafka" % "kafka-clients" % "2.4.0" exclude ("org.slf4j", "slf4j-log4j12")
   lazy val kafkaRelated = Seq(kafka)
 
   lazy val catsCore = "org.typelevel" %% "cats-core" % catsVersion
@@ -17,16 +17,16 @@ object Dependencies {
 
   lazy val simulacrum = "com.github.mpilquist" %% "simulacrum" % "0.19.0"
 
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.0"
-  lazy val mockitoCore = "org.mockito" % "mockito-core" % "3.1.0"
+  lazy val scalaTest   = "org.scalatest" %% "scalatest"   % "3.1.0"
+  lazy val mockitoCore = "org.mockito"   % "mockito-core" % "3.1.0"
 
   private val logbackVersion = "1.2.3"
-  lazy val logbackClass = "ch.qos.logback" % "logback-core" % logbackVersion
-  lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % logbackVersion
-  lazy val logbackRelated = Seq(logbackClass, logbackClassic)
+  lazy val logbackClass      = "ch.qos.logback" % "logback-core" % logbackVersion
+  lazy val logbackClassic    = "ch.qos.logback" % "logback-classic" % logbackVersion
+  lazy val logbackRelated    = Seq(logbackClass, logbackClassic)
 
-  lazy val silencerVersion = "1.4.4"
-  lazy val silencerPlugin = "com.github.ghik" % "silencer-plugin" % silencerVersion cross CrossVersion.full
-  lazy val kindProjector = "org.typelevel"  % "kind-projector" % "0.11.0" cross CrossVersion.full
+  lazy val silencerVersion = "1.6.0"
+  lazy val silencerPlugin  = "com.github.ghik" % "silencer-plugin" % silencerVersion cross CrossVersion.full
+  lazy val kindProjector   = "org.typelevel" % "kind-projector" % "0.11.0" cross CrossVersion.full
 
 }
