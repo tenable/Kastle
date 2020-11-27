1853cae3e02cd55fe8e25ef67136554df20fbe8e
Scala Steward <me@scala-steward.org>
2020-11-27 23:40:53 +0100

    Update cats-effect to 2.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   lazy val catsCore = "org.typelevel" %% "cats-core" % catsVersion
   lazy val catsFree = "org.typelevel" %% "cats-free" % catsVersion
 
-  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "2.2.0"
+  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "2.3.0"
 
   lazy val simulacrum = "org.typelevel" %% "simulacrum" % "1.0.1"
 
